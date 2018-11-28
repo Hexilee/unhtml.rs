4f4784e1291fa409e054b7404ff18a4e69534eff
Hexilee <hexileee@gmail.com>
2018-11-28 23:26:39 +0800

    complete iter_by_selector_and_inner_text

--- unhtml_util/src/lib.rs
+++ unhtml_util/src/lib.rs
@@ -13,73 +13,93 @@ pub const EQUAL_PUNCT: char = '=';
 pub const ROOT_SELECTOR: &str = "root";
 pub const ATTR_INNER_TEXT: &str = "innerText";
 
+use std::collections::LinkedList;
 pub use scraper::Selector;
 pub use scraper::Html;
 pub use scraper::ElementRef;
+pub use scraper::element_ref::Select;
 pub use std::str::FromStr;
 pub use failure::Error;
+pub use std::collections::linked_list::IntoIter;
 use self::err::ParseError;
 
-pub fn get_elem_by_selector_and_attr<E, T>(selector_str: &'static str, attr: &'static str) -> Box<Fn(ElementRef) -> Result<T, Error>>
-    where E: std::error::Error + Send + Sync + 'static,
-          T: FromStr<Err=E> {
-    let selector = Selector::parse(selector_str).unwrap();
-    Box::new(move |elem: ElementRef| {
-        let first_elem = elem.select(&selector).next().ok_or(
-            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-        )?;
-        Ok(T::from_str(first_elem.value().attr(attr).ok_or(
-            ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
-        )?)?)
-    })
+pub trait FromHtml {
+    type Err: std::error::Error + Send + Sync + 'static;
+    type Elem: FromStr<Err=Self::Err>;
+    fn get_elem_by_selector_and_attr(selector_str: &'static str, attr: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+        let selector = Selector::parse(selector_str).unwrap();
+        Box::new(move |elem: ElementRef| {
+            let first_elem = elem.select(&selector).next().ok_or(
+                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+            )?;
+            Ok(Self::Elem::from_str(first_elem.value().attr(attr).ok_or(
+                ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
+            )?)?)
+        })
+    }
+
+    fn get_elem_by_selector_and_inner_text(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+        let selector = Selector::parse(selector_str).unwrap();
+        Box::new(move |elem: ElementRef| {
+            let first_elem = elem.select(&selector).next().ok_or(
+                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+            )?;
+            Ok(Self::Elem::from_str(&first_elem.inner_html())?)
+        })
+    }
+
+    fn get_elem_by_selector_and_html(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+        let selector = Selector::parse(selector_str).unwrap();
+        Box::new(move |elem: ElementRef| {
+            let first_elem = elem.select(&selector).next().ok_or(
+                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+            )?;
+            Ok(Self::Elem::from_str(&first_elem.html())?)
+        })
+    }
+
+    fn get_elem_by_attr(attr: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+        Box::new(move |elem: ElementRef| {
+            Ok(Self::Elem::from_str(elem.value().attr(attr).ok_or(
+                ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
+            )?)?)
+        })
+    }
 }
 
-pub fn get_elem_by_selector_and_inner_text<E, T>(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<T, Error>>
-    where E: std::error::Error + Send + Sync + 'static,
-          T: FromStr<Err=E> {
-    let selector = Selector::parse(selector_str).unwrap();
-    Box::new(move |elem: ElementRef| {
-        let first_elem = elem.select(&selector).next().ok_or(
-            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-        )?;
-        Ok(T::from_str(&first_elem.inner_html())?)
-    })
-}
+pub trait IterFromHtml {
+    type Err: std::error::Error + Send + Sync + 'static;
+    type Elem: FromStr<Err=Self::Err> + 'static;
+    fn iter_from_single_str<Fun>(string: &'static str, getter_fn: Fun) -> Box<Fn(Select) -> Result<IntoIter<Self::Elem>, Error>>
+        where Fun: Fn(&'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> + 'static + Copy {
+        Box::new(move |selects| {
+            let mut list: LinkedList<Self::Elem> = LinkedList::new();
+            for elem_ref in selects {
+                list.push_back(getter_fn(string)(elem_ref)?);
+            }
+            Ok(list.into_iter())
+        })
+    }
 
-pub fn get_elem_by_selector_and_html<E, T>(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<T, Error>>
-    where E: std::error::Error + Send + Sync + 'static,
-          T: FromStr<Err=E> {
-    let selector = Selector::parse(selector_str).unwrap();
-    Box::new(move |elem: ElementRef| {
-        let first_elem = elem.select(&selector).next().ok_or(
-            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-        )?;
-        Ok(T::from_str(&first_elem.html())?)
-    })
-}
-
-pub fn get_elem_by_attr<E, T>(attr: &'static str) -> Box<Fn(ElementRef) -> Result<T, Error>>
-    where E: std::error::Error + Send + Sync + 'static,
-          T: FromStr<Err=E> {
-    Box::new(move |elem: ElementRef| {
-        Ok(T::from_str(elem.value().attr(attr).ok_or(
-            ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
-        )?)?)
-    })
-}
-
-pub trait VecFromStr<T: std::str::FromStr> {
-    fn vec_from_str(str_vec: Vec<&str>) -> Result<Vec<T>, T::Err> {
-        let mut result = Vec::with_capacity(str_vec.len());
-        for string in str_vec.iter() {
-            result.push(T::from_str(*string)?)
-        }
-        Ok(result)
+    fn iter_by_selector_and_inner_text(selector_str: &'static str) -> Box<Fn(Select) -> Result<IntoIter<Self::Elem>, Error>> {
+        Self::iter_from_single_str(selector_str, Self::Elem::get_elem_by_selector_and_inner_text)
     }
 }
 
 
-impl<T: std::str::FromStr> VecFromStr<T> for T {}
+impl<E, T> FromHtml for T
+    where E: std::error::Error + Send + Sync + 'static,
+          T: FromStr<Err=E> {
+    type Err = E;
+    type Elem = T;
+}
+
+impl<E, T> IterFromHtml for T
+    where E: std::error::Error + Send + Sync + 'static,
+          T: FromStr<Err=E> + 'static {
+    type Err = E;
+    type Elem = T;
+}
 
 #[cfg(test)]
 mod tests {
