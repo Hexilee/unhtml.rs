01d159e204e0f71557d1d7ed6eea1fb6a1e83654
Hexilee <hexileee@gmail.com>
2018-11-28 21:00:54 +0800

    complete get_elem_by_selector_and_inner_text

--- unhtml_util/src/lib.rs
+++ unhtml_util/src/lib.rs
@@ -34,12 +34,15 @@ pub fn get_elem_by_selector_and_attr<E, T>(selector_str: &'static str, attr: &'s
     })
 }
 
-pub fn get_elem_by_selector_and_inner_text(selector: &str) -> Box<Fn(&str) -> Option<String>> {
-    let selector = Selector::parse(selector).unwrap();
-    Box::new(move |html: &str| {
-        let doc = Html::parse_fragment(html);
-        let first_elem = doc.select(&selector).next()?;
-        Some(first_elem.inner_html().to_string())
+pub fn get_elem_by_selector_and_inner_text<E, T>(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<T, Error>>
+    where E: std::error::Error + Send + Sync + 'static,
+          T: FromStr<Err=E> {
+    let selector = Selector::parse(selector_str).unwrap();
+    Box::new(move |elem: ElementRef| {
+        let first_elem = elem.select(&selector).next().ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+        )?;
+        Ok(T::from_str(&first_elem.inner_html())?)
     })
 }
 
