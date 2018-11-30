6d8485a06572c0047b10ed8a4ce01032f5475632
Hexilee <hexileee@gmail.com>
2018-12-01 04:01:10 +0800

    pass all test of VecFromHtml

--- unhtml_util/src/test/vec_from_html.rs
+++ unhtml_util/src/test/vec_from_html.rs
@@ -1,102 +1,8 @@
 use super::super::*;
 use scraper::Html;
-use scraper::Selector;
 
 #[test]
-fn test_vec_by_selector_and_attr() {
-    let html = Html::parse_fragment(r#"
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <div id="test">
-        <div>
-            <a href="1"></a>
-        </div>
-        <div>
-            <a href="2"></a>
-        </div>
-        <div>
-            <a href="3"></a>
-        </div>
-    </div>
-</body>
-</html>
-    "#);
-    let selector = Selector::parse("#test > div").unwrap();
-    let results = u8::vec_by_selector_and_attr("a", "href")(html.select(&selector)).unwrap();
-    assert_eq!(1u8, results[0]);
-    assert_eq!(2u8, results[1]);
-    assert_eq!(3u8, results[2]);
-}
-
-#[test]
-fn test_vec_by_selector_and_inner_text() {
-    let html = Html::parse_fragment(r#"
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <div id="test">
-        <div>
-            <a>1</a>
-        </div>
-        <div>
-            <a>2</a>
-        </div>
-        <div>
-            <a>3</a>
-        </div>
-    </div>
-</body>
-</html>
-    "#);
-    let selector = Selector::parse("#test > div").unwrap();
-    let results = u8::vec_by_selector_and_inner_text("a")(html.select(&selector)).unwrap();
-    assert_eq!(1u8, results[0]);
-    assert_eq!(2u8, results[1]);
-    assert_eq!(3u8, results[2]);
-}
-
-#[test]
-fn test_vec_by_selector_and_html() {
-    let html = Html::parse_fragment(r#"
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <div id="test">
-        <div>
-            <a href="1"></a>
-        </div>
-        <div>
-            <a href="2"></a>
-        </div>
-        <div>
-            <a href="3"></a>
-        </div>
-    </div>
-</body>
-</html>
-    "#);
-    let selector = Selector::parse("#test > div").unwrap();
-    let results = String::vec_by_selector_and_html("a")(html.select(&selector)).unwrap();
-    assert_eq!(r#"<a href="1"></a>"#, results[0]);
-    assert_eq!(r#"<a href="2"></a>"#, results[1]);
-    assert_eq!(r#"<a href="3"></a>"#, results[2]);
-}
-
-#[test]
-fn test_vec_by_attr() {
+fn test_from_attr() {
     let html = Html::parse_fragment(r#"
     <!DOCTYPE html>
 <html lang="en">
@@ -113,15 +19,38 @@ fn test_vec_by_attr() {
 </body>
 </html>
     "#);
-    let selector = Selector::parse("#test > a").unwrap();
-    let results = u8::vec_by_attr("href")(html.select(&selector)).unwrap();
+    let results = Vec::<u8>::from_attr("#test > a", "href", html.root_element_ref()).unwrap();
     assert_eq!(1u8, results[0]);
     assert_eq!(2u8, results[1]);
     assert_eq!(3u8, results[2]);
 }
 
 #[test]
-fn test_vec_by_inner_text() {
+fn test_from_html() {
+    let html = Html::parse_fragment(r#"
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <div id="test">
+        <a href="1"></a>
+        <a href="2"></a>
+        <a href="3"></a>
+    </div>
+</body>
+</html>
+    "#);
+    let results = Vec::<String>::from_html("#test > a", html.root_element_ref()).unwrap();
+    assert_eq!(r#"<a href="1"></a>"#, results[0]);
+    assert_eq!(r#"<a href="2"></a>"#, results[1]);
+    assert_eq!(r#"<a href="3"></a>"#, results[2]);
+}
+
+#[test]
+fn test_from_inner_text() {
     let html = Html::parse_fragment(r#"
     <!DOCTYPE html>
 <html lang="en">
@@ -138,34 +67,8 @@ fn test_vec_by_inner_text() {
 </body>
 </html>
     "#);
-    let selector = Selector::parse("#test > a").unwrap();
-    let results = u8::vec_by_inner_text(html.select(&selector)).unwrap();
+    let results = Vec::<u8>::from_inner_text("#test > a", html.root_element_ref()).unwrap();
     assert_eq!(1u8, results[0]);
     assert_eq!(2u8, results[1]);
     assert_eq!(3u8, results[2]);
-}
-
-#[test]
-fn test_vec_by_html() {
-    let html = Html::parse_fragment(r#"
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-    <div id="test">
-        <a>1</a>
-        <a>2</a>
-        <a>3</a>
-    </div>
-</body>
-</html>
-    "#);
-    let selector = Selector::parse("#test > a").unwrap();
-    let results = String::vec_by_html(html.select(&selector)).unwrap();
-    assert_eq!("<a>1</a>", results[0]);
-    assert_eq!("<a>2</a>", results[1]);
-    assert_eq!("<a>3</a>", results[2]);
 }
