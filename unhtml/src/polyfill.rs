18beef5c074c83f0c318fae76630c393ad6f89f7
Hexilee <hexileee@gmail.com>
2018-12-01 18:54:55 +0800

    update polyfill

--- unhtml/src/polyfill.rs
+++ unhtml/src/polyfill.rs
@@ -2,13 +2,13 @@ use scraper::Html;
 use scraper::ElementRef;
 
 pub trait RootElementRef {
-    fn root_element_ref(&self) -> ElementRef;
+    fn root_element(&self) -> ElementRef;
 }
 
 impl RootElementRef for Html {
     /// Get root element_ref `html`
     /// remove when new version of scraper published
-    fn root_element_ref(&self) -> ElementRef {
+    fn root_element(&self) -> ElementRef {
         let root_node = self.tree.root().first_child().unwrap();
         ElementRef::wrap(root_node).unwrap()
     }
@@ -22,7 +22,7 @@ mod tests {
     #[test]
     fn test_html_root_element_ref() {
         let html = Html::parse_fragment(r#"<a href="http://github.com">1</a>"#);
-        let root_ref = html.root_element_ref();
+        let root_ref = html.root_element();
         let href = root_ref.select(&Selector::parse("a").unwrap()).next().unwrap();
         assert_eq!(href.inner_html(), "1");
         assert_eq!(href.value().attr("href").unwrap(), "http://github.com");
