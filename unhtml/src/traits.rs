0ddd3084a7ec1cfb2f9a293f265538d1b1b96626
Hexilee <hexileee@gmail.com>
2018-12-01 19:18:23 +0800

    unhtml cargo build successfully

--- unhtml/src/traits.rs
+++ unhtml/src/traits.rs
@@ -1,68 +1,58 @@
 use scraper::Selector;
 use scraper::ElementRef;
-use scraper::html::Select;
 use std::str::FromStr;
 use failure::Error;
 use super::err::ParseError;
 
-pub trait FromHtml {
-    type Err: std::error::Error + Send + Sync + 'static;
-    type Elem: FromStr<Err=Self::Err>;
-    fn get_elem_by_selector_and_attr(selector_str: &'static str, attr: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+pub trait FromHtml: Sized {
+    fn from_selector_and_attr(selector_str: &str, attr: &str, elem_ref: ElementRef) -> Result<Self, Error> {
         let selector = Selector::parse(selector_str).unwrap();
-        Box::new(move |elem: ElementRef| {
-            let first_elem = elem.select(&selector).next().ok_or(
-                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-            )?;
-            Ok(Self::Elem::from_str(first_elem.value().attr(attr).ok_or(
-                ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
-            )?)?)
-        })
+        let first_elem = elem_ref.select(&selector).next().ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+        )?;
+        Ok(Self::from_html(first_elem.value().attr(attr).ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
+        )?)?)
     }
 
-    fn get_elem_by_selector_and_inner_text(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+    fn from_selector_and_inner_text(selector_str: &str, elem_ref: ElementRef) -> Result<Self, Error> {
         let selector = Selector::parse(selector_str).unwrap();
-        Box::new(move |elem: ElementRef| {
-            let first_elem = elem.select(&selector).next().ok_or(
-                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-            )?;
-            Ok(Self::Elem::from_str(&first_elem.inner_html())?)
-        })
+        let first_elem = elem_ref.select(&selector).next().ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+        )?;
+        Ok(Self::from_html(&first_elem.inner_html())?)
     }
 
-    fn get_elem_by_selector_and_html(selector_str: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
+    fn from_selector_and_html(selector_str: &str, elem_ref: ElementRef) -> Result<Self, Error> {
         let selector = Selector::parse(selector_str).unwrap();
-        Box::new(move |elem: ElementRef| {
-            let first_elem = elem.select(&selector).next().ok_or(
-                ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
-            )?;
-            Ok(Self::Elem::from_str(&first_elem.html())?)
-        })
+        let first_elem = elem_ref.select(&selector).next().ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "selector".to_string(), value: selector_str.to_string() }
+        )?;
+        Ok(Self::from_html(&first_elem.html())?)
     }
 
-    fn get_elem_by_attr(attr: &'static str) -> Box<Fn(ElementRef) -> Result<Self::Elem, Error>> {
-        Box::new(move |elem: ElementRef| {
-            Ok(Self::Elem::from_str(elem.value().attr(attr).ok_or(
-                ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
-            )?)?)
-        })
+    fn from_attr(attr: &str, elem_ref: ElementRef) -> Result<Self, Error> {
+        Ok(Self::from_html(elem_ref.value().attr(attr).ok_or(
+            ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
+        )?)?)
     }
 
-    fn get_elem_by_inner_text(elem_ref: ElementRef) -> Result<Self::Elem, Error> {
-        Ok(Self::Elem::from_str(&elem_ref.inner_html())?)
+    fn from_inner_text(elem_ref: ElementRef) -> Result<Self, Error> {
+        Ok(Self::from_html(&elem_ref.inner_html())?)
     }
 
-    fn get_elem_by_html(elem_ref: ElementRef) -> Result<Self::Elem, Error> {
-        Ok(Self::Elem::from_str(&elem_ref.html())?)
+    fn from_html_ref(elem_ref: ElementRef) -> Result<Self, Error> {
+        Ok(Self::from_html(&elem_ref.html())?)
     }
+
+    fn from_html(html: &str) -> Result<Self, Error>;
 }
 
 pub trait VecFromHtml {
-    type Err: std::error::Error + Send + Sync + 'static;
-    type Elem: FromStr<Err=Self::Err> + 'static;
+    type Elem: FromHtml;
     fn from_attr(selector_str: &str, attr: &str, root_element_ref: ElementRef) -> Result<Vec<Self::Elem>, Error> {
         Self::vec_from(selector_str, root_element_ref, |element_ref| {
-            Ok(Self::Elem::from_str(
+            Ok(Self::Elem::from_html(
                 element_ref.value().attr(attr).ok_or(
                     ParseError::SelectOrAttrEmptyErr { attr: "attr".to_string(), value: attr.to_string() }
                 )?
@@ -72,13 +62,13 @@ pub trait VecFromHtml {
 
     fn from_inner_text(selector_str: &str, root_element_ref: ElementRef) -> Result<Vec<Self::Elem>, Error> {
         Self::vec_from(selector_str, root_element_ref, |element_ref| {
-            Ok(Self::Elem::from_str(&element_ref.inner_html())?)
+            Ok(Self::Elem::from_html(&element_ref.inner_html())?)
         })
     }
 
     fn from_html(selector_str: &str, root_element_ref: ElementRef) -> Result<Vec<Self::Elem>, Error> {
         Self::vec_from(selector_str, root_element_ref, |element_ref| {
-            Ok(Self::Elem::from_str(&element_ref.html())?)
+            Ok(Self::Elem::from_html(&element_ref.html())?)
         })
     }
 
@@ -98,13 +88,12 @@ pub trait VecFromHtml {
 impl<E, T> FromHtml for T
     where E: std::error::Error + Send + Sync + 'static,
           T: FromStr<Err=E> {
-    type Err = E;
-    type Elem = T;
+    fn from_html(html: &str) -> Result<Self, Error> {
+        Ok(T::from_str(html)?)
+    }
 }
 
-impl<E, T> VecFromHtml for Vec<T>
-    where E: std::error::Error + Send + Sync + 'static,
-          T: FromStr<Err=E> + 'static {
-    type Err = E;
+impl<T> VecFromHtml for Vec<T>
+    where T: FromHtml {
     type Elem = T;
 }
