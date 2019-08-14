3ea3f459802982d12a16ee184227065c6652e852
Hexilee <hexileee@gmail.com>
2019-08-14 22:58:31 +0800

    add test derive-html; expand fails

---
+++ unhtml/derive-tests/html.rs
@@ -0,0 +1,125 @@
+use unhtml::{
+    scraper::{Html, Selector},
+    Element, Result,
+};
+
+use unhtml::derive::FromHtml;
+
+#[derive(Debug, Eq, PartialEq, FromHtml)]
+struct Link {
+    #[html(attr = "href")]
+    href: String,
+
+    #[html(attr = "inner")]
+    text: String,
+}
+
+#[test]
+fn test_element() {
+    let selector = Selector::parse("a").unwrap();
+    let html = Html::parse_fragment(
+        r##"
+        <div>
+            <div>
+                <a href="https://github.com"> Github </a>
+            </div>
+            <div>
+                <a href="https://www.zjuqsc.com"> ZJU QSC </a>
+            </div>
+            <a href="https://google.com"> Google </a>
+        </div>
+    "##,
+    );
+    assert_eq!(
+        Link {
+            href: "https://github.com".into(),
+            text: "Github".into(),
+        },
+        html.select(&selector).element().unwrap()
+    );
+}
+
+#[test]
+fn test_vec_element() {
+    let selector = Selector::parse("a").unwrap();
+    let html = Html::parse_fragment(
+        r##"
+        <div>
+            <div>
+                <a href="https://github.com"> Github </a>
+            </div>
+            <div>
+                <a href="https://www.zjuqsc.com"> ZJU QSC </a>
+            </div>
+            <a href="https://google.com"> Google </a>
+        </div>
+    "##,
+    );
+
+    let actual: Vec<Link> = html.select(&selector).element().unwrap();
+    assert_eq!(
+        actual,
+        vec![
+            Link {
+                href: "https://github.com".into(),
+                text: "Github".into(),
+            },
+            Link {
+                href: "https://www.zjuqsc.com".into(),
+                text: "ZJU QSC".into(),
+            },
+            Link {
+                href: "https://google.com".into(),
+                text: "Google".into(),
+            },
+        ]
+    );
+}
+
+#[test]
+fn test_fail_element() {
+    let foo_selector = Selector::parse("p").unwrap();
+    let html = Html::parse_fragment(
+        r##"
+        <div>
+            <div>
+                <a href="https://github.com"> Github </a>
+            </div>
+            <div>
+                <a href="https://www.zjuqsc.com"> ZJU QSC </a>
+            </div>
+            <a href="https://google.com"> Google </a>
+        </div>
+    "##,
+    );
+    let result: Result<Link> = html.select(&foo_selector).element();
+    assert!(!result.is_ok());
+}
+
+#[test]
+fn test_option_element() {
+    let selector = Selector::parse("a").unwrap();
+    let foo_selector = Selector::parse("p").unwrap();
+    let html = Html::parse_fragment(
+        r##"
+        <div>
+            <div>
+                <a href="https://github.com"> Github </a>
+            </div>
+            <div>
+                <a href="https://www.zjuqsc.com"> ZJU QSC </a>
+            </div>
+            <a href="https://google.com"> Google </a>
+        </div>
+    "##,
+    );
+    assert_eq!(
+        Some(Link {
+            href: "https://github.com".into(),
+            text: "Github".into(),
+        }),
+        html.select(&selector).element().unwrap()
+    );
+    let result: Option<Link> = html.select(&foo_selector).element().unwrap();
+    assert_eq!(result, None);
+}
